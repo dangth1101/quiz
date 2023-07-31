39ecadcdee32579cd9f4b1a92a6039d1964cdc7e
dangth1101 <dangth.dmc@gmail.com>
2023-07-31 19:43:55 +0700

    UX/UI MainPage

--- lib/src/widgets/elevated_card.dart
+++ lib/src/widgets/elevated_card.dart
@@ -18,48 +18,47 @@ class _ElevatedCardState extends State<ElevatedCard> {
       child: InkWell(
         onTap: () {
           _style = !_style;
-          print("changed");
           setState(() {});
         },
         child: SizedBox(
           width: 160,
-          child: _style
-              ? Column(
-                  children: [
-                    Padding(
-                      padding: const EdgeInsets.only(
-                        left: 8.0,
-                        right: 8.0,
-                        top: 20.0,
-                      ),
-                      child: Text(
-                        "Title",
-                        style: Theme.of(context).textTheme.titleLarge,
-                        maxLines: 1,
-                        overflow: TextOverflow.ellipsis,
-                      ),
-                    ),
-                    Divider(
-                      indent: 16,
-                      endIndent: 16,
-                      thickness: 2,
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.symmetric(horizontal: 8.0),
-                      child: Text(
-                        "This is a sample decription\n\n\n",
-                        textAlign: TextAlign.center,
-                        style: Theme.of(context).textTheme.bodyLarge,
-                        overflow: TextOverflow.ellipsis,
-                        maxLines: 3,
-                      ),
-                    ),
-                    Divider(
-                      indent: 16,
-                      endIndent: 16,
-                      thickness: 1,
-                    ),
-                    Padding(
+          child: Column(
+            children: [
+              Padding(
+                padding: const EdgeInsets.only(
+                  left: 8.0,
+                  right: 8.0,
+                  top: 10.0,
+                ),
+                child: Text(
+                  "Title",
+                  style: Theme.of(context).textTheme.titleLarge,
+                  maxLines: 1,
+                  overflow: TextOverflow.ellipsis,
+                ),
+              ),
+              const Divider(
+                indent: 16,
+                endIndent: 16,
+                thickness: 2,
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                child: Text(
+                  "This is a sample decription\n\n\n",
+                  textAlign: TextAlign.center,
+                  style: Theme.of(context).textTheme.bodyLarge,
+                  overflow: TextOverflow.ellipsis,
+                  maxLines: 3,
+                ),
+              ),
+              const Divider(
+                indent: 16,
+                endIndent: 16,
+                thickness: 1,
+              ),
+              _style
+                  ? Padding(
                       padding: const EdgeInsets.symmetric(horizontal: 8.0),
                       child: Text(
                         "#HashTag",
@@ -69,88 +68,57 @@ class _ElevatedCardState extends State<ElevatedCard> {
                         maxLines: 1,
                       ),
                     )
-                  ],
-                )
-              : Column(
-                  children: [
-                    Align(
-                      alignment: Alignment.topRight,
-                      child: InkWell(
-                        onTap: () {},
-                        child: Icon(
-                          Icons.cancel,
-                          color: Colors.red,
-                        ),
+                  : Container(
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: [
+                          Container(
+                            height: 30,
+                            width: 30,
+                            decoration: BoxDecoration(
+                              shape: BoxShape.circle,
+                              color: Colors.green,
+                            ),
+                            child: Center(
+                              child: Icon(
+                                Icons.play_arrow,
+                                color: Colors.white,
+                              ),
+                            ),
+                          ),
+                          Container(
+                            height: 30,
+                            width: 30,
+                            decoration: BoxDecoration(
+                              shape: BoxShape.circle,
+                              color: Colors.orangeAccent,
+                            ),
+                            child: Center(
+                              child: Icon(
+                                Icons.edit,
+                                color: Colors.white,
+                              ),
+                            ),
+                          ),
+                          Container(
+                            height: 30,
+                            width: 30,
+                            decoration: BoxDecoration(
+                              shape: BoxShape.circle,
+                              color: Colors.red,
+                            ),
+                            child: Center(
+                              child: Icon(
+                                Icons.delete,
+                                color: Colors.white,
+                              ),
+                            ),
+                          ),
+                        ],
                       ),
                     ),
-                    Padding(
-                      padding: const EdgeInsets.only(left: 8.0, right: 40.0),
-                      child: TextButton(
-                        style: TextButton.styleFrom(
-                          backgroundColor: Colors.green,
-                        ),
-                        onPressed: () {},
-                        child: Row(
-                          children: [
-                            Icon(
-                              Icons.flag,
-                              color: Colors.white,
-                            ),
-                            VerticalDivider(),
-                            Text(
-                              "START",
-                              style: TextStyle(color: Colors.white),
-                            ),
-                          ],
-                        ),
-                      ),
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.only(left: 8.0, right: 40.0),
-                      child: TextButton(
-                        style: TextButton.styleFrom(
-                          backgroundColor: Colors.blue,
-                        ),
-                        onPressed: () {},
-                        child: Row(
-                          children: [
-                            Icon(
-                              Icons.bookmark_add,
-                              color: Colors.white,
-                            ),
-                            VerticalDivider(),
-                            Text(
-                              "SAVE ",
-                              style: TextStyle(color: Colors.white),
-                            ),
-                          ],
-                        ),
-                      ),
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.only(left: 8.0, right: 10.0),
-                      child: TextButton(
-                        style: TextButton.styleFrom(
-                          backgroundColor: Colors.blue,
-                        ),
-                        onPressed: () {},
-                        child: Row(
-                          children: [
-                            Icon(
-                              Icons.forum,
-                              color: Colors.white,
-                            ),
-                            VerticalDivider(),
-                            Text(
-                              "COMMENT",
-                              style: TextStyle(color: Colors.white),
-                            ),
-                          ],
-                        ),
-                      ),
-                    ),
-                  ],
-                ),
+            ],
+          ),
         ),
       ),
     );
